273684cb9a8358c5f41bfc8cad4c52931124b583
Karina Flores Mendez <karinamendez762@gmail.com>
2022-04-29 20:21:57 -0400

    commit

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template 
+app = Flask(__name__)    
+@app.route('/')          
+def checkerboard1():
+    return render_template("index.html", row=8, col=8, color1='black', color2='red') 
+
+@app.route('/<int:x>')
+def row(x):
+    return render_template("index.html",row=x,col=8,color1='red',color2='black')
+
+@app.route('/<int:x>/<int:y>')
+def row_col(x,y):
+    return render_template("index.html",row=x,col=y,color1='red',color2='black')
+
+@app.route('/<int:x>/<int:y>/<string:one>')
+def row_col_one(x,y,one):
+    return render_template("index.html",row=x,col=y,color1=one,color2='black')
+
+@app.route('/<int:x>/<int:y>/<string:one>/<string:two>')
+def row_col_two(x,y,one,two):
+    return render_template("index.html",row=x,col=y,color1=one,color2=two)
+
+if __name__=="__main__":      
+    app.run(debug=True)    
